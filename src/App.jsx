b0b444fde316191a453287b19cb1790fdb052365
Giovanna de Moraes <gigimoraessol@gmail.com>
2025-05-24 11:17:18 -0300

    routes between pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>Context</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<h1>Home</h1>} />
+          <Route path="/products" element={<h1>Products</h1>} />
+          <Route path="/about" element={<h1>About</h1>} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
